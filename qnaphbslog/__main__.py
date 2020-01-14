8f7c725e46b4b7ea0e4b3756700e2bfd46b3e921
tungyuehlin <tungyuehlin@qnap.com>
2020-01-14 17:56:28 +0800

    sync_history: fix no history and no exception bug

--- qnaphbslog/__main__.py
+++ qnaphbslog/__main__.py
@@ -33,14 +33,12 @@ def main():
         print_hbs_summary(hbs)
 
     if args.sync_history:
-        for root, dirs, files in os.walk(hbs_log_path):
-            if not root.endswith('system'):
+        jobs = get_jobs(hbs_log_path)
+        for job in jobs:
+            if job.job_type != 'sync':
                 continue
-            for dir in dirs:
-                job_path = os.path.join(root, dir)
-                job_log_path = os.path.join(job_path, 'log')
-                print(f'Job Name: {dir}')
-                analyze_sync_history(job_log_path)
+            job_log_path = get_job_log_path(hbs_log_path, job.name)
+            print_sync_history(job.name, job_log_path)
 
 
 def get_hbs_version(hbs_log_path):
@@ -110,7 +108,18 @@ def print_hbs_summary(hbs: HybridBackupSync):
             print(f'  name: {job.name}, provider type: {account.provider_type}')
 
 
-def analyze_sync_history(log_path):
+def get_job_log_path(hbs_log_path, job_name):
+    for root, dirs, files in os.walk(hbs_log_path):
+        if not root.endswith('system'):
+            continue
+        for dir in dirs:
+            if dir == job_name:
+                job_path = os.path.join(root, dir)
+                job_log_path = os.path.join(job_path, 'log')
+                return job_log_path
+
+
+def print_sync_history(job_name, log_path):
     if not os.path.exists(log_path):
         print(f'{log_path} not exists')
         return
@@ -121,7 +130,7 @@ def analyze_sync_history(log_path):
         return
 
     job_history = get_job_history(history_paths)
-    print_sync_history_report(job_history)
+    print_sync_history_report(job_name, job_history)
 
 
 def get_history_paths(log_path):
@@ -151,16 +160,23 @@ def get_job_history(history_paths) -> SyncFileHistory:
     return job_history
 
 
-def print_sync_history_report(job_history):
-    print(f'Job History from {job_history.start_time()} to {job_history.end_time()}')
+def print_sync_history_report(job_name, job_history):
+    print(f'Job Name: {job_name}')
+    if job_history.total_files() == 0:
+        print('No Sync History')
+        return
+    print(f'Sync History from {job_history.start_time()} to {job_history.end_time()}')
     print(f'Total files: {job_history.total_files()}')
     print(f'Total folders: {job_history.total_folders()}')
     print('Action summary:')
     for action in job_history.action_types():
         print(f'  {len(job_history.get_files_by_action(action))} try to {action}')
-    print('Exception summary:')
-    for exception in job_history.exception_types():
-        print(f'  {exception}: {len(job_history.get_files_by_exception(exception))}')
+    if len(job_history.exception_types()) == 0:
+        print('No exception')
+    else:
+        print('Exception summary:')
+        for exception in job_history.exception_types():
+            print(f'  {exception}: {len(job_history.get_files_by_exception(exception))}')
 
 
 if __name__ == '__main__':
